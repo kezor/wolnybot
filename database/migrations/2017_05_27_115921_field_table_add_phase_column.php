882c0f7633efca5075fc2f2a22e623de863b06a7
Maciej Roszkiewicz <maciek@roszkiewicz.info>
2017-05-27 14:17:48 +0200

    Add phase column into fields table

---
+++ database/migrations/2017_05_27_115921_field_table_add_phase_column.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+
+class FieldTableAddPhaseColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fields', function($table) {
+            $table->integer('phase');
+        });
+    }offset_y
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('fields', function($table) {
+            $table->dropColumn('phase');
+        });
+    }
+}
