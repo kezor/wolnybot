ab68089b39f2e0de48f83dcccec2d021736eb8a8
Maciej Roszkiewicz <git@roszkiewicz.info>
2017-06-07 14:30:45 +0200

    remove overlapping

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,8 +36,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->command('bot:test')
-                  ->everyFiveMinutes()
-                  ->withoutOverlapping();
+                  ->everyFiveMinutes();
     }
 
     /**
