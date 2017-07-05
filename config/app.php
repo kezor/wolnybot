8683b032fdf638518c3653ceb436ac3d9a3c9f6e
Maciej Roszkiewicz <git@roszkiewicz.info>
2017-07-05 12:18:29 +0200

    Add log-viewer

--- config/app.php
+++ config/app.php
@@ -120,7 +120,7 @@ return [
     |
     */
 
-    'log' => env('APP_LOG', 'single'),
+    'log' => env('APP_LOG', 'daily'),
 
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         \Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        \Arcanedev\LogViewer\LogViewerServiceProvider::class,
 
     ],
 
