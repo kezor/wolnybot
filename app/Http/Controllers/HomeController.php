45fda576915a731a183681e7977f88e024ade55e
Maciej Roszkiewicz <maciej.roszkiewicz@gmail.com>
2018-06-05 21:28:59 +0200

    Remove commented line

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
     public function index()
     {
         $players = Player::where('user_id', Auth::id())->get();
-//        Auth::id();
+
         return view('home', [
             'players' => $players
         ]);
