bb4892e90c19c09c1c7e7dd5351d4f19576ac461
Maciej Roszkiewicz <maciej.roszkiewicz@gmail.com>
2018-07-01 18:00:13 +0200

    Clean up

--- app/Building/Farmland.php
+++ app/Building/Farmland.php
@@ -13,8 +13,6 @@ use App\Space;
 
 class Farmland extends Space
 {
-    private $usedSeeds = [];
-
     /**
      * @var Field[]
      */
@@ -48,99 +46,6 @@ class Farmland extends Space
         }
 
         return $fieldsToSeed;
-    }
-
-//    private function selectFields($fields, Product $product)
-//    {
-//        reset($fields);
-//        $index = key($fields);
-//
-//        $finalFieldsAvailableToSeed = [];
-//
-//        while (isset($fields[$index])) {
-//            $availableToSeed = true;
-//
-//            for ($xIndex = 0; $xIndex < $product->getLength(); $xIndex++) {
-//                for ($yIndex = 0; $yIndex < $product->getHeight(); $yIndex++) {
-//                    $checkingIndex = $index + $xIndex + ($yIndex * 12);
-//                    if (!isset($fields[$checkingIndex]) || $this->isNextIndexInNextRow($index, $checkingIndex)) {
-//                        $availableToSeed = false;
-//                    }
-//                }
-//            }
-//
-//            if (!$availableToSeed) {
-//                unset($fields[$index]);
-//            } else {
-//                $finalFieldsAvailableToSeed[$index] = clone $fields[$index];
-//                $indexesToRemove = $this->getIndexesToRemove($index, $product);
-//                foreach ($indexesToRemove as $indexToRemove) {
-//                    unset($fields[$indexToRemove]);
-//                }
-//                if ($product->getAmount() <= count($finalFieldsAvailableToSeed)) {
-//                    break;
-//                }
-//            }
-//            reset($fields);
-//            $index = key($fields);
-//        }
-//
-//        /** @var Field $field */
-//        foreach ($finalFieldsAvailableToSeed as $field) {
-//            $field->setProduct($product);
-//        }
-//
-//        return $finalFieldsAvailableToSeed;
-//    }
-
-//    private function getIndexesToRemove($currentIndex, Product $plant)
-//    {
-//        $indexes = [];
-//
-//        for ($i = 0; $i < $plant->getLength(); $i++) {
-//            for ($j = 0; $j < $plant->getHeight(); $j++) {
-//                $indexToRemove = $currentIndex + $i + (12 * $j);
-//                $indexes[$indexToRemove] = $indexToRemove;
-//            }
-//        }
-//
-//        return $indexes;
-//    }
-
-//    private function isNextIndexInNextRow($index, $nextIndex)
-//    {
-//        $currentColumn = $this->getColumn($index);
-//        $nextColumn = $this->getColumn($nextIndex);
-//
-//        return $nextColumn < $currentColumn;
-//    }
-
-//    private function getColumn($index)
-//    {
-//        $column = $index % 12;
-//        if ($column == 0) {
-//            $column = 12;
-//        }
-//
-//        return $column;
-//    }
-
-    private function getProductToSeed()
-    {
-        /** @var Product $stockProduct */
-        $stockProduct = Product::where('player_id', $this->player_id)
-            ->where('amount', '>', 0)
-            ->whereIn('pid', ProductCategoryMapper::getVegetablesPids())
-            ->whereNotIn('pid', $this->usedSeeds)
-            ->orderBy('amount', 'ASC')
-            ->first();
-        if (!$stockProduct) {
-            return null;
-        }
-
-        $this->usedSeeds[] = $stockProduct->pid;
-
-        return $stockProduct;
     }
 
     public function clearFields($updatedIndexed)
